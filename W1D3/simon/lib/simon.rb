495ac7b039de33458e40b5d1cec8247e1db3ecd3
brucetangdesign <bruce@brucetangdesign.com>
2020-04-23 13:22:24 -0400

    Finished Simon

--- W1D3/simon/lib/simon.rb
+++ W1D3/simon/lib/simon.rb
@@ -13,9 +13,10 @@ class Simon
   end
 
   def play
-    take_turn #until @game_over
+    take_turn until @game_over
     
     if @game_over
+      system("clear")
       game_over_message
       reset_game
     end
@@ -23,30 +24,35 @@ class Simon
 
   def take_turn
     show_sequence
-
+    sleep(2)
+    system("clear")
     require_sequence
+    
     if !@game_over
       round_success_message
       @sequence_length += 1
+      sleep(1)
+      system("clear")
     end
   end
 
   def show_sequence
     add_random_color
-    system("clear")
     
     @seq.each { |col| puts col.colorize(col.to_sym)}
   end
 
   def require_sequence
-    ans_seq = []
-    puts "Type the next color:"
-    #until ans_seq.length == @seq.length
+    step_num = 0
+    
+    (0...@sequence_length).each do |i|
+      puts "Type the next color:"
       ans = gets.chomp
-      ans_seq << ans
-
-      puts "correct" if ans_seq == @seq[0...ans_seq.length]
-    #end
+      if ans != @seq[i]
+        @game_over = true
+        break
+      end
+    end
   end
 
   def add_random_color
